141c6ed582934761d744cdd6c9047d10b0e575c7
linlianfu <1241352445@qq.com>
2018-01-02 16:37:05 +0800

    提交mq的测试单元

---
+++ src/test/java/cn/llf/mq/Consumer.java
@@ -0,0 +1,55 @@
+package cn.llf.mq;
+
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
+import com.alibaba.rocketmq.common.message.Message;
+import com.alibaba.rocketmq.common.message.MessageExt;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * 创建者：   linlf
+ * 创建时间： 2017/12/29
+ * 描述：
+ */@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/mq.xml")
+public class Consumer {
+
+
+    @Test
+    public  void consumerTest(){
+        DefaultMQPushConsumer consumer =
+                new DefaultMQPushConsumer("PushConsumer");
+        consumer.setNamesrvAddr("192.168.25.242:9876");
+        try {
+            //订阅PushTopic下Tag为push的消息
+            consumer.subscribe("PushTopic", "push");
+//            //程序第一次启动从消息队列头取数据
+            consumer.setConsumeFromWhere(
+                    ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+            consumer.registerMessageListener(
+                    new MessageListenerConcurrently() {
+                        public ConsumeConcurrentlyStatus consumeMessage(
+                                List<MessageExt> list,
+                                ConsumeConcurrentlyContext Context) {
+                            Message msg = list.get(0);
+                            System.out.println(msg.toString());
+                            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                        }
+                    }
+            );
+            consumer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/cn/llf/mq/Producer.java
@@ -0,0 +1,52 @@
+package cn.llf.mq;
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.common.message.Message;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 创建者：   linlf
+ * 创建时间： 2017/12/29
+ * 描述：
+ */
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/mq.xml")
+public class Producer {
+
+
+    @Test
+    public void testLog(){
+        log.info("2342222222");
+//        System.out.println("123213");
+    }
+
+    @Test
+    public void producerTest(){
+
+        DefaultMQProducer producer = new DefaultMQProducer("Producer");
+        producer.setNamesrvAddr("192.168.25.242:9876");
+        try {
+            producer.start();
+            for (int i = 0 ; i< 3 ; i++){
+                Message msg = new Message("PushTopic",
+                        "push",
+                         i+"",
+                        "Just for test.".getBytes());
+
+                SendResult result = producer.send(msg);
+                System.out.println("id:" + result.getMsgId() +
+                        " result:" + result.getSendStatus());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            producer.shutdown();
+        }
+    }
+
+}
