1e22420aea225198e94f4a646e16815b0554e1e7
linlianfu <1241352445@qq.com>
2018-10-28 14:18:21 +0800

    dubbo调用超时时间加大 分布式锁实现探究代码

--- src/main/java/cn/llf/framework/gateway/web/admin/UserManagerAction.java
+++ src/main/java/cn/llf/framework/gateway/web/admin/UserManagerAction.java
@@ -3,12 +3,16 @@ package cn.llf.framework.gateway.web.admin;
 import cn.eleven.basic.data.user.south.api.IUserQueryService;
 import cn.eleven.basic.data.user.south.api.arg.UserQuery;
 import cn.eleven.basic.data.user.south.api.dto.UserBaseDto;
+import cn.llf.framework.services.Lock;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -16,15 +20,61 @@ import java.util.List;
  * @date: 2018/9/1 21:55
  * @description: 用户管理控制器
  */
+@Slf4j
 @RestController
 @RequestMapping("userManager")
 public class UserManagerAction {
 
     @Autowired
-    IUserQueryService userQueryService;
+    private IUserQueryService userQueryService;
+
+    @Autowired
+    private Lock lock;
 
     @GetMapping("listBase")
-    public List<UserBaseDto> listBase(UserQuery query){
-      return   Collections.emptyList();
+    public List<UserBaseDto> listBase(@ModelAttribute UserQuery query){
+        InterProcessMutex interProcessMutex = lock.getInterProcessMutex("user");
+        try {
+            interProcessMutex.acquire();
+            Collection<String> participantNodes = interProcessMutex.getParticipantNodes();
+            log.warn("{}", participantNodes);
+        } catch (Exception e) {
+            log.error("获取锁失败");
+            e.printStackTrace();
+        }
+        log.warn("name:{},region:{}",query.getName(),query.getRegion());
+
+        List<UserBaseDto> userBaseDtos = userQueryService.listBase(query);
+        List<UserBaseDto> secondList = userQueryService.listBase(query);
+        List<UserBaseDto> thirdList = userQueryService.listBase(query);
+        try {
+            interProcessMutex.release();
+        } catch (Exception e) {
+            log.info("释放锁失败");
+            e.printStackTrace();
+        }
+        return   userQueryService.listBase(query);
+    }
+    @GetMapping("lock")
+    public List<UserBaseDto> lock(@ModelAttribute UserQuery query){
+        InterProcessMutex interProcessMutex = lock.getInterProcessMutex("lock");
+//        try {
+//            interProcessMutex.acquire();
+//        } catch (Exception e) {
+//            log.error("获取锁失败");
+//            e.printStackTrace();
+//        }
+        log.warn("name:{},region:{}",query.getName(),query.getRegion());
+
+        List<UserBaseDto> userBaseDtos = userQueryService.listBase(query);
+        List<UserBaseDto> secondList = userQueryService.listBase(query);
+        List<UserBaseDto> thirdList = userQueryService.listBase(query);
+//        try {
+//            interProcessMutex.release();
+//        } catch (Exception e) {
+//            log.info("释放锁失败");
+//            e.printStackTrace();
+//        }
+        return   userQueryService.listBase(query);
     }
 }
---
+++ src/main/java/cn/llf/framework/services/Lock.java
@@ -0,0 +1,48 @@
+package cn.llf.framework.services;
+
+import cn.eleven.common.except.BasicRuntimeException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author eleven
+ * @date 2018/10/13
+ * @description  zookeeper分布式锁
+ */
+@Slf4j
+@Service
+public class Lock implements InitializingBean{
+
+
+    private RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3,2);
+
+    private CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181",2000,2000, retryPolicy);
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+        client.start();
+        log.info(">>>>>启动分布式锁的zk创建");
+    }
+
+    /**
+     * 获取指定节点的锁
+     * @return
+     */
+    public InterProcessMutex getInterProcessMutex(String node){
+
+        if (StringUtils.isBlank(node))
+            throw new BasicRuntimeException("锁节点不能为空");
+
+        return new InterProcessMutex(client, "/curator/lock"+"/"+node);
+    }
+
+
+}
