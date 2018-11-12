bafdad4b3b2b8c2a42261f47ab266c03d3bbb54c
linlianfu <1241352445@qq.com>
2018-11-12 20:41:10 +0800

    spring allicationContext学习

--- src/test/java/cn/llf/spring/applicationContext/ApplicationContextTest.java
+++ src/test/java/cn/llf/spring/applicationContext/ApplicationContextTest.java
@@ -17,11 +17,16 @@ public class ApplicationContextTest {
 
     /**
      * {@link ClassPathXmlApplicationContext}以类路径的方式加载启动spring ioc容器
+     * 从容器种获取bean方法：根据类回去，根据xml配置的id获取，根据xml配置的name获取
      */
     @Test
     public void test(){
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring/applicationContext.xml");
-        Car bean = context.getBean(Car.class);
-        bean.introduce();
+        ApplicationContext context = new ClassPathXmlApplicationContext("/spring/applicationContext.xml");
+        Car getBeanById = (Car)context.getBean("carId");
+        Car getBeanByClass = context.getBean(Car.class);
+        Car getBeanByName = (Car)context.getBean("carName");
+        getBeanById.introduce();
+        getBeanByClass.introduce();
+        getBeanByName.introduce();
     }
 }
