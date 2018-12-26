ac044305539dd9ff0b6adf8ab397732dce2b4323
linlianfu <1241352445@qq.com>
2018-12-26 21:40:20 +0800

    多线程案例测试

--- src/test/java/cn/llf/thread/AsyncThread.java
+++ src/test/java/cn/llf/thread/AsyncThread.java
@@ -2,6 +2,8 @@ package cn.llf.thread;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author eleven
  * @date 2018/12/25
@@ -10,36 +12,97 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AsyncThread {
 
-    public static final Object lock = new Object();
+    static Object lock = new Object();
 
-//    public static void main(String[] arg){
-//        Object lock = new Object();
-//        MyThread myThread = new MyThread(lock,"t1");
-//        synchronized (myThread){
-//            myThread.start();
+
+
+//    @Test
+
+    /**
+     * 测试线程和唤醒
+     */
+//    public  static void main(String[] arg){
+//        MyThread myThread = new MyThread("线程1",lock);
+//        myThread.start();
+//
+//        synchronized (lock){
+//            log.info("主线程执行");
 //            try {
-//                myThread.wait(2000);
+//                lock.wait(4000);
 //            } catch (InterruptedException e) {
 //                e.printStackTrace();
 //            }
-//            log.info("主线程继续执行");
+//            log.info(">>>>>主线程继续执行");
 //        }
+//        Thread.currentThread().interrupt();
 //    }
 
-    public static void main(String[] args){
-        MyThread t1 = new MyThread("t1");
-        //启动“线程1”
-        log.info("主线程启动异步线程");
-        t1.start();
-        synchronized (lock) {
+    /**
+     * 测试线程中断 - 正常中断
+     * @param arg
+     */
+//    public static void main(String[] arg){
+//        Object lock = new Object();
+//        MyThread myThread = new MyThread("线程中断测试",lock);
+//
+//        log.info(">>>>>【主线程】启动【子线程】");
+//        myThread.start();
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(100);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        log.info(">>>>>【主线程】中断【子线程】");
+//        myThread.interrupt();
+//        log.info(">>>>>主线程运行结束");
+//    }
 
-            try {
-                lock.wait(3000);
-                log.info("主线程继续执行");
-                } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    /**
+     * 测试线程中断 - 阻塞中断异常
+     * @param arg
+     */
+//    public static void main(String[] arg){
+//        Object lock = new Object();
+//        MyThread myThread = new MyThread("线程中断测试",lock);
+//
+//        log.info(">>>>>【主线程】启动【子线程】");
+//        myThread.start();
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(100);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        log.info(">>>>>【主线程】中断【子线程】");
+//        myThread.interrupt();
+//        log.info("子线程异常停止之后，判断子线程是否还是活动状态，myThread.isAlive()：【{}】",myThread.isAlive());
+//
+//        if (myThread.isAlive()){
+//            log.info("主线程调用中断子线程之后，子线程没有正常停止，异常发生！！！！！！！");
+//        }
+//        log.info(">>>>>主线程运行结束");
+//    }
+    /**
+     * 测试线程中断 - 阻塞中断正常
+     * @param arg
+     */
+    public static void main(String[] arg){
+        Object lock = new Object();
+        MyThread myThread = new MyThread("线程中断测试",lock);
+
+        log.info(">>>>>【主线程】启动【子线程】");
+        myThread.start();
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    }
+        log.info(">>>>>【主线程】中断【子线程】");
+        myThread.interrupt();
+        log.info("子线程异常停止之后，判断子线程是否还是活动状态，myThread.isAlive()：【{}】",myThread.isAlive());
 
+        if (myThread.isAlive()){
+            log.info("主线程调用中断子线程之后，子线程没有正常停止，异常发生！！！！！！！");
+        }
+        log.info(">>>>>主线程运行结束");
+    }
 }
--- src/test/java/cn/llf/thread/MyThread.java
+++ src/test/java/cn/llf/thread/MyThread.java
@@ -10,30 +10,109 @@ import java.util.concurrent.TimeUnit;
  * @description
  */
 @Slf4j
-public class MyThread extends Thread {
+public class MyThread extends Thread{
 
+    String name;
     Object lock;
 
-    String threadName;
-
-    MyThread(String threadName){
-        this.threadName = threadName;
+    MyThread(String name,Object lock){
+        this.name = name;
+        this.lock = lock;
     }
 
+    /**
+     * 测试线程和唤醒
+     */
+//    public void run(){
+//        log.info("异步线程【{}】开始执行,延时5s",name);
+//        try {
+//            TimeUnit.SECONDS.sleep(10);
+//            log.info("通知唤醒等待的线程");
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        //把代码块也整合到同步代块内，会导致该子线程会一直占用lock锁，
+//        //导致主线程无法获取lock的对象锁，所以主线程设置lock.wait(5000)5S超时会无效
+//        synchronized (lock){
+//            log.info("异步线程执行结束");
+//            //wait和notify等方法必须拥有的对象锁，否则会抛出 IllegalMonitorStateException异常，
+//            // 表示当前线程没有拥有该对象锁，确调用过了wait和notify等方法
+//            lock.notify();
+//        }
+//    }
+
+
+    /**
+     * 测试线程中断 - 正常中断
+     */
+//    @Override
+//    public void run() {
+//        log.info(">>>>>【子线程】开始执行");
+//
+//        for (int i=0;i<10000;i++){
+//            if (!this.isInterrupted())
+//            log.info(i+"");
+//        }
+//        log.info(">>>>>【子线程】运行结束");
+//
+//    }
+    /**
+     * 测试线程中断 - 阻塞中断异常
+     *
+     * 主线程试图停止一个正处于阻塞的子线程，系统除了会抛出InterruptedException异常外，还会调用interrupted()函数，
+     * 调用时能获取到中断状态是true的状态，调用完之后会复位中断状态为false，
+     * 所以异常抛出之后通过isInterrupted()是获取不到中断状态是true的状态，从而不能退出循环
+     */
+//    @Override
+//    public void run() {
+//        log.info(">>>>>【子线程】开始执行");
+//        log.info("【子线程启动睡眠等待】");
+////        for (int i=0; i<100;i++ ){
+////            log.info(""+i);
+////        }
+//        while (!this.isInterrupted()){
+//            //模拟子线程处于阻塞状态
+//            try {
+//                TimeUnit.SECONDS.sleep(5);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        log.info(">>>>>【子线程】运行结束");
+//    }
+
+    /**
+     * 测试线程中断-阻塞中断正常
+     */
     @Override
     public void run() {
-        log.info("异步任务【{}】开始执行，设置延时5S",threadName);
-//        while (true);
-        synchronized (this){
+        log.info(">>>>>【子线程】开始执行");
+        log.info(">>>>>【子线程】启动睡眠等待");
+//        for (int i=0; i<100;i++ ){
+//            log.info(""+i);
+//        }
+        while (!this.isInterrupted()){
+            //模拟子线程处于阻塞状态
             try {
+                logInfo();
                 TimeUnit.SECONDS.sleep(5);
             } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            synchronized (AsyncThread.lock){
-                AsyncThread.lock.notify();
+                //try-catch之后，通过break正确停止子线程
+                log.error(">>>>>阻塞中断异常,通过break正确停止子线程");
+                break;
             }
         }
-        log.info("异步任务结束，唤醒主线程");
+        log.info(">>>>>【子线程】运行结束");
+    }
+
+
+    public void logInfo(){
+        log.info("方法延时");
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            log.info("》》》》》线程延时异常");
+        }
     }
 }
