1a866d421b25ab86902e8b434ae49a677279fd51
linlianfu <1241352445@qq.com>
2019-03-23 15:50:32 +0800

    zookeeper监听机制及API调用

--- src/test/java/cn/llf/zookeeper/ZookeeperUse.java
+++ src/test/java/cn/llf/zookeeper/ZookeeperUse.java
@@ -1,11 +1,10 @@
 package cn.llf.zookeeper;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -13,6 +12,10 @@ import java.util.concurrent.TimeUnit;
  * @author eleven
  * @date 2019/3/22
  * @description 测试java api 操作zookeeper
+ *
+ * 对应笔记参考:
+ *      1:<zookeeper系统学习笔记>
+ *      1:<zookeeper API操作及watch监听机制>
  */
 @Slf4j
 public class ZookeeperUse implements Watcher {
@@ -61,47 +64,195 @@ public class ZookeeperUse implements Watcher {
     //************************会话复用 start**************************************
 
 
+//    public static void main(String[] arg){
+//        try {
+//            ZooKeeper zooKeeper = new ZooKeeper(connectString,sessionTimeout,new ZookeeperUse());
+//            try {
+//                countDownLatch.await();
+//
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//
+//            long sessionId = zooKeeper.getSessionId();
+//            byte[] sessionPasswd = zooKeeper.getSessionPasswd();
+//
+//            //测试错误的sessionId连接失败
+//            //监听时间输出：收到server的监听事件：WatchedEvent state:Expired type:None path:null
+//            zooKeeper = new ZooKeeper(connectString,sessionTimeout,new ZookeeperUse(),1L,sessionPasswd);
+//
+//            //测试正确的sessionId和sessionPasswd可以复用连接成功
+//            zooKeeper = new ZooKeeper(connectString,sessionTimeout,new ZookeeperUse(),sessionId,sessionPasswd);
+//
+//            try {
+//                TimeUnit.SECONDS.sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
+//    @Override
+//    public void process(WatchedEvent event) {
+//        log.info("收到server的监听事件：{}",event);
+//        if (event.getState() == Event.KeeperState.SyncConnected){
+//            countDownLatch.countDown();
+//        }
+//    }
+
+
+    //************************会话复用 end**************************************
+
+
+
+
+    //************************适用api 创建节点start**************************************
+
+
+//    public static void main(String[] arg){
+//        try {
+//            ZooKeeper zooKeeper = new ZooKeeper(connectString,sessionTimeout,new ZookeeperUse());
+//            countDownLatch.await();
+//
+//            //返回创建成功的path
+//            String path = zooKeeper.create("/zk-test-", "测试api创建节点数据".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+//            log.info("成功创建临时节点znode:{}",path);
+//
+//            //创建临时有序的节点，server会默认加上一个数字床
+//            String temp = zooKeeper.create("/zk-test-", "测试api创建节点数据".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+//            log.info("成功创建临时有序的节点znode:{}",temp);
+//
+//
+//            //通过延时，可以查看zk节点上是否这边指定的节点，因为是临时节点，一旦client断开连接，节点将自动删除
+//            TimeUnit.SECONDS.sleep(10);
+//
+//
+//            //上述是同步创建方案，zookeeper还提供了异步方法，提供一个回调接口即可
+//            /**
+//             * {@link ZooKeeper#create(java.lang.String, byte[], java.util.List, org.apache.zookeeper.CreateMode, org.apache.zookeeper.AsyncCallback.StringCallback, java.lang.Object) }
+//             **/
+//
+//
+//
+//            //测试创建永久节点
+//            String persistentPath = zooKeeper.create("/test-delete", "im data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+//            log.info(">>>>>成功创建永久节点:{}",persistentPath);
+//        } catch (IOException | InterruptedException | KeeperException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    public void process(WatchedEvent event) {
+//        log.info("收到server监听时间:{}",event);
+//        if (event.getState() == Event.KeeperState.SyncConnected){
+//            countDownLatch.countDown();
+//        }
+//    }
+
+
+    //************************适用api 创建节点end**************************************
+
+    //************************删除节点start**************************************
+
+
+
+//    public static void main(String[] arg){
+//        ZooKeeper zooKeeper = null;
+//        try {
+//            zooKeeper = new ZooKeeper(connectString,sessionTimeout,new ZookeeperUse());
+//            //数据版本
+//            zooKeeper.delete("/test-delete",0);
+//        } catch (IOException | InterruptedException | KeeperException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+
+    //************************删除节点end**************************************
+
+
+
+    //************************获取节点，并测试监听机制start**************************************
+
+    private static ZooKeeper zooKeeper = null;
+
+    private static String parentPath = "/zk-test";
+
     public static void main(String[] arg){
+
         try {
-            ZooKeeper zooKeeper = new ZooKeeper(connectString,sessionTimeout,new ZookeeperUse());
-            try {
-                countDownLatch.await();
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            zooKeeper = new ZooKeeper(connectString,sessionTimeout,new ZookeeperUse());
 
 
-            long sessionId = zooKeeper.getSessionId();
-            byte[] sessionPasswd = zooKeeper.getSessionPasswd();
+            countDownLatch.await();
+            //创建节点
+            String testPathParent = zooKeeper.create(parentPath, "test-data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 
-            //测试错误的sessionId连接失败
-            //监听时间输出：收到server的监听事件：WatchedEvent state:Expired type:None path:null
-            zooKeeper = new ZooKeeper(connectString,sessionTimeout,new ZookeeperUse(),1L,sessionPasswd);
+            //获取子节点信息，并同时注册该节点的监听
+            List<String> children = zooKeeper.getChildren(parentPath, new ZookeeperUse());
+            log.info("节点【{}】下的子节点列表【{}】",parentPath,children);
 
-            //测试正确的sessionId和sessionPasswd可以复用连接成功
-            zooKeeper = new ZooKeeper(connectString,sessionTimeout,new ZookeeperUse(),sessionId,sessionPasswd);
+            //延时1S，完成监听注册在继续走流程
+            TimeUnit.SECONDS.sleep(1);
 
-            try {
-                TimeUnit.SECONDS.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            //创建子节点,此时会触发节点监听通知机制
+            String testPathSon = zooKeeper.create(parentPath+"/son", "test-data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 
-        } catch (IOException e) {
+            //延时2S，等待收到节点变化通知之后再结束流程
+            TimeUnit.SECONDS.sleep(2);
+
+
+        } catch (IOException | InterruptedException | KeeperException e) {
             e.printStackTrace();
         }
-    }
 
+
+    }
 
     @Override
     public void process(WatchedEvent event) {
-        log.info("收到server的监听事件：{}",event);
-        if (event.getState() == Event.KeeperState.SyncConnected){
-            countDownLatch.countDown();
+        log.info("收到server监听通知:{}",event);
+        if (Event.KeeperState.SyncConnected == event.getState()){
+            if (event.getType() == Event.EventType.NodeChildrenChanged){
+                try {
+                    log.info(">>>>>收到子节点变化通知,尝试重新获取节点信息:【{}】",zooKeeper.getChildren(event.getPath(),true));
+                } catch (KeeperException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }else {
+                log.info("收到zookeeper创建连接监听通知");
+                countDownLatch.countDown();
+            }
+
         }
     }
 
+    //************************获取节点，并测试监听机制end**************************************
 
-    //************************会话复用 start**************************************
+
+
+
+
+
+
+
+    /**
+     * 通用的实现类
+     * @param event
+     */
+
+//    @Override
+//    public void process(WatchedEvent event) {
+//        log.info("收到server监听时间:{}",event);
+//        if (event.getState() == Event.KeeperState.SyncConnected){
+//            countDownLatch.countDown();
+//        }
+//    }
 }
