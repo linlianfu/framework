dd6ae824cf21555ee63e2e2b1fb8c20f67b60038
linlianfu <1241352445@qq.com>
2019-03-24 15:06:11 +0800

    zkClient监听子节点变化

--- src/test/java/cn/llf/zookeeper/ZkClientDemo.java
+++ src/test/java/cn/llf/zookeeper/ZkClientDemo.java
@@ -2,8 +2,15 @@ package cn.llf.zookeeper;
 
 import lombok.extern.slf4j.Slf4j;
 import org.I0Itec.zkclient.ZkClient;
+import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.ZooKeeper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -13,36 +20,81 @@ import java.util.concurrent.CountDownLatch;
  *
  */
 @Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:common.xml")
 public class ZkClientDemo {
 
 
-    private static ZooKeeper zooKeeper;
-    private static String zkTestPath = "/zk-test";
-    private static String path = "/zk-test-auth";
-    private static String pathSon = "/zk-test-auth/test";
-    private static CountDownLatch countDownLatch = new CountDownLatch(1);
-    private static String connectString = "127.0.0.1";
-    private static  int sessionTimeout = 5000;
-    private static String auth = "foo:true";
-    private static String scheme = "digest";
+    private  ZooKeeper zooKeeper;
+    private  String zkTestPath = "/zk-test";
+    private  String path = "/zk-test-auth";
+    private  String pathSon = "/zk-test-auth/test";
+    private  CountDownLatch countDownLatch = new CountDownLatch(1);
+    private  String connectString = "127.0.0.1";
+    private  int sessionTimeout = 5000;
+    private  String auth = "foo:true";
+    private  String scheme = "digest";
+    private ZkClient client = null;
 
 
 
-    public static void main(String[] arg){
-        //会话建立
-        ZkClient client = new ZkClient(connectString,5000);
-        log.info(">>>>>会话建立");
+    @Before
+    public void initClient(){
+        client = new ZkClient(connectString,5000);
+        log.info("客户端会话建立成功");
+    }
+
+    /**
+     * 基本API操作
+     */
+
+    @Test
+    public void basicOperation(){
         //湖区子节点
-//        List<String> children = client.getChildren(zkTestPath);
-//        log.info(">>>>>【{}】子节点【{}】",zkTestPath,children);
+        List<String> children = client.getChildren(zkTestPath);
+        log.info(">>>>>【{}】子节点【{}】",zkTestPath,children);
 
-//        String path = client.create("/test-zk-client-create", "init", CreateMode.PERSISTENT);
-//        log.info("创建成功的节点间:【{}】",path);
+        String path = client.create("/test-zk-client-create", "init", CreateMode.PERSISTENT);
+        log.info("创建成功的节点间:【{}】",path);
 
-//        client.createPersistent("/parent/son/money",true);
-//        log.info(">>>>>递归创建节点");
+        client.createPersistent("/parent/son/money",true);
+        log.info(">>>>>递归创建节点");
 
         Object o = client.readData("/test-zk-client-create");
         log.info("读取到的节点数据:{}",o.toString());
     }
+
+    /**
+     * 获取子节点并监听子节点变化
+     */
+    @Test
+    public void getChildrenAndSubscribeChildChanges(){
+        log.info("测试获取子节点同时并绑定监听");
+        List<String> children = client.getChildren(zkTestPath);
+        log.info("【{}】的子节点列表",children);
+
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        client.subscribeChildChanges(zkTestPath,(parentPath,currentChildList)->{
+            //currentChildList:当前的子节点列表
+            log.info("收到节点【{}】的子节点变化通知，当前子节点:【{}】",parentPath,currentChildList);
+            countDownLatch.countDown();
+        });
+
+        client.createPersistent(zkTestPath+"/clientCreate2","i'm data");
+        log.info("创建子节点【{}】成功","clientCreate2");
+
+
+
+        client.delete(zkTestPath+"/son2");
+        log.info("成功删除子节点【son2】");
+
+
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
