b44b2282ab243940823c88ff2c24e7abda110aab
linlianfu <1241352445@qq.com>
2019-07-24 23:17:44 +0800

    增加注解

--- src/test/java/cn/llf/javabase/HashCodeAction.java
+++ src/test/java/cn/llf/javabase/HashCodeAction.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 /**
  * @author: eleven
- * @since: 2018/5/22 15:19
+ * @since: 2018/5/22 15:19 测试git命令提交
  * @description:
  */
 @Slf4j
@@ -35,6 +35,9 @@ public class HashCodeAction {
         log.info(String.valueOf("对象equals是否相等:"+math.equals(chinese)));
         //==比较的是是否完全是同一个对象，也就是是否在同一片内存空间上
         //而math和chinese存的分别是指向不同堆内对象的reference。
+        //obj1==obj2 判断是obj1,obj2这两个引用变量是否相等，即它们所指向的对象是否为同一个对象。
+        // 言外之意就是要求两个变量所指内存地址相等的时候，才能返回true，每个对象都有自己的一块内存，
+        // 因此必须指向同一个对象才返回ture。
         log.info(String.valueOf("对象=否相等:"+(math==chinese)));
 
 
