5f6f9d18759bb560899be34ab31f4af761fef6ad
linlianfu <1241352445@qq.com>
2020-06-25 16:55:58 +0800

    feat: 增加redistemplat模板

---
+++ src/test/java/cn/llf/redis/RedisDemo.java
@@ -0,0 +1,35 @@
+package cn.llf.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundHashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Set;
+
+/**
+ * @author eleven
+ * @date 2020/6/25
+ * @description
+ */
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring/redis.xml")
+public class RedisDemo {
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+    @Test
+    public void getAllKey(){
+        BoundHashOperations<String, Object, Object> java = redisTemplate.boundHashOps("java");
+        Set<Object> keys = java.keys();
+        for (Object key : keys) {
+            log.warn(">> key:{}",key);
+        }
+
+    }
+}
